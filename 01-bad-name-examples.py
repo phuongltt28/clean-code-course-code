2d4b8681cf875576398fdca0b683f17b8798b5b9
Phuong Le Thi Truc <lephuong139@gmail.com>
2023-05-27 12:49:53 +0700

    Naming improvement

--- 01-bad-name-examples.py
+++ 01-bad-name-examples.py
@@ -1,24 +1,22 @@
 from datetime import datetime
 
 
-class Entity:
-    def __init__(self, title, description, ymdhm):
+class BlogPost:
+    def __init__(self, title, description, date_published):
         self.title = title
         self.description = description
-        self.ymdhm = ymdhm
+        self.date_published = date_published
+
+    def print(self):
+        print('Title: ' + self.title)
+        print('Description: ' + self.description)
+        print('Published: ' + self.date_published)
 
 
-def output(item):
-    print('Title: ' + item.title)
-    print('Description: ' + item.description)
-    print('Published: ' + item.ymdhm)
+title = 'Clean Code Is Great!'
+description = 'Actually, writing Clean Code can be pretty fun. You\'ll see!'
+now = datetime.now()
+formatted_date = now.strftime('%Y-%m-%d %H:%M')
 
-
-summary = 'Clean Code Is Great!'
-desc = 'Actually, writing Clean Code can be pretty fun. You\'ll see!'
-new_date = datetime.now()
-publish = new_date.strftime('%Y-%m-%d %H:%M')
-
-item = Entity(summary, desc, publish)
-
-output(item)
+post = BlogPost(title, description, formatted_date)
+post.print()
